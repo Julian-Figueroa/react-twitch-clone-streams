41f4ed07250dd6a381e26988eb10141e891d83d7
https://julian-figueroa@bitbucket.org <julianfigueroa@MacBook-Pro-de-Julian.local>
2019-08-15 09:06:48 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className='ui container'>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={StreamList} />
+          <Route exact path='/streams/new' component={StreamCreate} />
+          <Route exact path='/streams/edit' component={StreamEdit} />
+          <Route exact path='/streams/delete' component={StreamDelete} />
+          <Route exact path='/streams/show' component={StreamShow} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
